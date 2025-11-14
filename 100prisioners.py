7cfdb7253e0a72fd11abc97c0ac656a7c507d687
kkkMorg <Morg.k.0104@gmail.com>
2025-11-14 17:05:44 +0900

    feat: Add main function

--- 100prisioners.py
+++ 100prisioners.py
@@ -1,7 +1,19 @@
 import random
 
 def main():
-    pass
+    NUM_DRAWERS = 10
+    NUM_REPETITIONS = int(1E5)
+
+    print('{:15}: {:5} ({})'.format('approach', 'wins', 'ratio'))
+    for approach in PrisionersGame.approaches:
+        num_victories = 0
+        for _ in range(NUM_REPETITIONS):
+            game = PrisionersGame(NUM_DRAWERS)
+            num_victories += PrisionersGame.victory(game.play(approach))
+
+        print('{:15}: {:5} ({:.2%})'.format(
+            approach.__name__, num_victories, num_victories / NUM_REPETITIONS))
+
 
 class PrisionersGame:
     """docstring for PrisionersGame"""
