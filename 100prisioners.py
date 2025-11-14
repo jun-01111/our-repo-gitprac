897153f6c41e8e85bcf062df99426c19a4e1f92a
kkkMorg <Morg.k.0104@gmail.com>
2025-11-14 16:57:43 +0900

    feat: Add main

---
+++ 100prisioners.py
@@ -0,0 +1,12 @@
+import random
+
+def main():
+    pass
+
+class PrisionersGame:
+    """docstring for PrisionersGame"""
+    def __init__(self, num_drawers):
+        pass
+
+if __name__ == '__main__':
+    main()
